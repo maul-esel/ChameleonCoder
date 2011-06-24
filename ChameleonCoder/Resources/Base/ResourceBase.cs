7b507d8ab05ead4d56ad2286e5582260469b0223
maul.esel <maul.esel@go4more.de>
2011-06-24 16:12:42 +0200

    renamed subfolders

---
+++ ChameleonCoder/Resources/Base/ResourceBase.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Xml;
+using ChameleonCoder.Resources.Collections;
+
+namespace ChameleonCoder.Resources.Base
+{
+    /// <summary>
+    /// an abstract class for resources
+    /// </summary>
+    public abstract partial class ResourceBase
+    {
+        /// <summary>
+        /// serves as base constructor for inherited classes and sets general properties
+        /// </summary>
+        /// <param name="xml">a XmlDocument object containing the entire document
+        /// in which the resource is defined.</param>
+        /// <param name="xpath">the XPath to the root element of the resource</param>
+        /// <param name="datafile">the file in which the resource is defined</param>
+        internal ResourceBase(ref XmlDocument xml, string xpath, string datafile)
+        {
+            this.XPath = xpath;
+            this.DataFile = datafile;
+            this.XML = xml;
+
+            this.GUID = new Guid(this.XML.SelectSingleNode(this.XPath + "/@guid").Value);
+
+            this.MetaData = new MetadataCollection();
+
+            int i = 0;
+            Metadata data;
+            try
+            {
+                foreach (XmlNode node in this.XML.SelectNodes(this.XPath + "/metadata"))
+                {
+                    i++;
+                    this.MetaData.Add(data = new Metadata(ref xml, this.XPath + "/metadata[" + i + "]"));
+                    if (data.IsDefault)
+                    {
+                        this.DefaultData = data;
+                    }
+                }
+            }
+            catch { }
+
+            this.children = new Collections.ResourceCollection();
+        }
+
+        #region properties
+
+        /// <summary>
+        /// the file that contains the resources definition
+        /// </summary>
+        internal string DataFile { get; private set; }
+
+        /// <summary>
+        /// the display name of the resource
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                try
+                {
+                    return this.XML.SelectSingleNode(this.XPath + "/@name").Value;
+                }
+                catch (NullReferenceException) { return string.Empty; }
+            }
+            protected internal set { this.XML.SelectSingleNode(this.XPath + "/@name").Value = value; }
+        }
+
+        /// <summary>
+        /// the GUID that uniquely identifies the resource
+        /// </summary>
+        public Guid GUID { get; private set; }
+
+        /// <summary>
+        /// defines the resource's type
+        /// </summary>
+        public ResourceType Type { get; protected internal set; }
+
+        /// <summary>
+        /// the XPath to the resource's definition in the datafile
+        /// </summary>
+        internal string XPath { get; private set; }
+
+        /// <summary>
+        /// contains the metadata marked as default
+        /// </summary>
+        protected internal Metadata DefaultData { get; set; }
+
+        /// <summary>
+        /// a short description of the resource
+        /// </summary>
+        public string Description
+        {
+            get { return this.XML.SelectSingleNode(this.XPath + "/@description").Value; }
+            protected internal set { this.XML.SelectSingleNode(this.XPath + "/@description").Value = value; }
+        }
+
+        /// <summary>
+        /// the associated metadata as Metadata class instances
+        /// </summary>
+        public MetadataCollection MetaData { get; protected internal set; }
+
+        /// <summary>
+        /// any notes related to the resource
+        /// </summary>
+        public string Notes
+        {
+            get
+            {
+                try { return this.XML.SelectSingleNode(this.XPath + "/@notes").Value; }
+                catch (NullReferenceException) { return string.Empty; }
+            }
+            set { this.XML.SelectSingleNode(this.XPath + "/@notes").Value = value; }
+        }
+
+        /// <summary>
+        /// the XmlDocument used to navigat through the resource's contents
+        /// </summary>
+        protected internal XmlDocument XML { get; set; }
+
+        /// <summary>
+        /// a ResourceCollection containing the children resources
+        /// </summary>
+        public Resources.Collections.ResourceCollection children { get; protected internal set; }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// opens a resource
+        /// </summary>
+        internal virtual void Open()
+        {
+            App.Gui.PropertyGrid.Items.Clear();
+
+            // maybe this could be done with binding and several data templates.
+            // Currently it doesn't work because the contents are not added (see comments).
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("Name"), this.Name }));
+
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("ResourceType"), ToString(this.Type) }));
+
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("Tree"), "/" + this.Node.FullPath }));
+
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("Description"), this.Description }));
+
+            if (this.DefaultData != null)
+                App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { this.DefaultData.GetName(), this.DefaultData.GetValue() }));
+
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("DataFile"), this.DataFile }));
+
+            App.Gui.PropertyGrid.Items.Add(new ListViewItem()); //new string[] { Localization.get_string("GUID"), this.GUID.ToString() }));
+        }
+
+        /// <summary>
+        /// packages a resource into a zip file,
+        /// including all attached or linked resources,
+        /// all data files,
+        /// any file a resource points to (FileResource, CodeResource, LibraryResource)
+        /// </summary>
+        internal virtual void Package()
+        {
+
+        }
+
+        /// <summary>
+        /// saves the information changed through the UI to the current instance and its XML representation
+        /// </summary>
+        internal virtual void Save()
+        {
+            this.XML.SelectSingleNode(this.XPath + "/@data-type").Value = ((int)this.Type).ToString();
+
+            foreach (Metadata data in this.MetaData)
+            {
+                data.Save(); // changes through the UI should be saved when they occur or through binding
+            }
+
+            System.IO.File.WriteAllText(this.DataFile, this.XML.DocumentElement.OuterXml);
+        }
+
+        /// <summary>
+        /// adds a metadata element, given any changes through the UI
+        /// it directly manipulates
+        /// 1) the XmlDocument
+        /// 2) the MetadataCollection
+        /// </summary>
+        internal virtual void AddMetadata()
+        {
+            // ...
+
+            this.Save();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal virtual void DeleteMetadata()
+        {
+
+        }
+
+        /// <summary>
+        /// attaches a copy of the resource to another resource
+        /// to do so, a new GUID is created.
+        /// a parameter or user input define where it goes:
+        /// the original resource or the new copy
+        /// </summary>
+        internal virtual void AttachResource() // todo: parameter to define where the guid goes
+        {
+
+        }
+
+        /// <summary>
+        /// receives a resource that should be attached,
+        /// adds it into the tree (-view), the children collection and the XmlDocument
+        /// </summary>
+        internal virtual void ReceiveAttach()
+        {
+
+        }
+
+        /// <summary>
+        /// links a resource to another resource
+        /// </summary>
+        internal virtual void LinkResource()
+        {
+
+        }
+
+        /// <summary>
+        /// receives a resource link
+        /// </summary>
+        internal virtual void ReceiveLink()
+        {
+
+        }
+
+        /// <summary>
+        /// moves a resource to another resource
+        /// by first copying and then deleting it.
+        /// When it is copied, the copy should receive the old GUID
+        /// </summary>
+        internal virtual void Move()
+        {
+
+        }
+
+        /// <summary>
+        /// deletes a resource by removing it from the tree (-view), any Collections and the XmlDocument
+        /// </summary>
+        internal virtual void Delete()
+        {
+            
+        }
+
+        /// <summary>
+        /// overrides object.ToString()
+        /// </summary>
+        /// <returns>the name of the resource</returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        #endregion
+    }
+}
---
+++ ChameleonCoder/Resources/Base/ResourceManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChameleonCoder.Resources.Collections;
+
+namespace ChameleonCoder.Resources.Base
+{
+    internal class ResourceManager
+    {
+        /// <summary>
+        /// contains all resources that don't have a direct parent (top-level resources)
+        /// </summary>
+        internal static ResourceCollection children;
+
+        /// <summary>
+        /// contains a list of ALL resources
+        /// </summary>
+        internal static ResourceCollection FlatList;
+
+        internal static ResourceBase ActiveItem;
+
+
+        /// <summary>
+        /// adds a resource
+        /// 1) to the list of ALL resources
+        /// 2) to a given parent list OR the list of top-level resources
+        /// </summary>
+        /// <param name="instance">the resource to add</param>
+        /// <param name="parentlist">the parent list to add the resource to.
+        /// If this is null, it will be added to the list of top-level resources</param>
+        internal static void Add(ResourceBase instance, ResourceCollection parentlist)
+        {
+            FlatList.Add(instance);
+
+            if (parentlist == null)
+            {
+                children.Add(instance);
+            }
+            else
+            {
+                parentlist.Add(instance);
+            }
+        }
+
+        // needed: remove methods
+    }
+}
