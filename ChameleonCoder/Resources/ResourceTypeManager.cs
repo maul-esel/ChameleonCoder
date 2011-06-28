6297328b41ae9fcb477e8b51edcd557c05122417
maul.esel <maul.esel@go4more.de>
2011-06-28 22:22:58 +0200

    added ResourceTypeManager class + IResource interface

---
+++ ChameleonCoder/Resources/ResourceTypeManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+
+namespace ChameleonCoder.Resources
+{
+    internal sealed class ResourceTypeManager
+    {
+        private static ResourceTypeCollection ResourceTypes = new ResourceTypeCollection();
+
+        internal static void Load()
+        {
+            var internTypes = from type in Assembly.GetEntryAssembly().GetTypes()
+                              where type.GetInterface(typeof(IResource).FullName) != null
+                              select type;
+
+            var externTypes = from dll in Directory.GetFiles(Environment.CurrentDirectory + "\\Components", "*.dll")
+                              let assembly = Assembly.LoadFrom(dll)
+                              from type in assembly.GetTypes()
+                              where type.GetInterface(typeof(IResource).FullName) != null
+                              select type;
+
+            var types = internTypes.Concat(externTypes);
+
+            foreach (Type type in types)
+                ResourceTypes.RegisterResourceType(type);
+
+        }
+    }
+}
