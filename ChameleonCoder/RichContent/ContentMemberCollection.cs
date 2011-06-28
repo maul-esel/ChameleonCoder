f88703f1fcbbfdd826ccaf561c1d20eb0cd944c5
maul.esel <maul.esel@go4more.de>
2011-06-28 21:58:39 +0200

    made ContentMemberCollection internal

--- ChameleonCoder/RichContent/ContentMemberCollection.cs
+++ ChameleonCoder/RichContent/ContentMemberCollection.cs
@@ -2,14 +2,14 @@
 
 namespace ChameleonCoder.RichContent
 {
-    public sealed class ContentMemberCollection : ComponentCollection<string, Type>
+    internal sealed class ContentMemberCollection : ComponentCollection<string, Type>
     {
-        public void RegisterMember(Type member)
+        internal void RegisterMember(Type member)
         {
             base.RegisterComponent((Activator.CreateInstance(member) as IContentMember).Alias, member);
         }
 
-        public Type GetMember(string alias)
+        internal Type GetMember(string alias)
         {
             return base.GetComponent(alias);
         }
