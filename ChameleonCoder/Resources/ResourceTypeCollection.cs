507c478cc083a9cb3928914faddf74397b47a27c
maul.esel <maul.esel@go4more.de>
2011-06-28 22:07:18 +0200

    added ResourceTypeCollection class

---
+++ ChameleonCoder/Resources/ResourceTypeCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChameleonCoder.Resources.Base;
+
+namespace ChameleonCoder.Resources
+{
+    internal sealed class ResourceTypeCollection : ComponentCollection<string, Type>
+    {
+        internal void RegisterResourceType(Type resourceType)
+        {
+            base.RegisterComponent((Activator.CreateInstance(resourceType) as ResourceBase).Alias, resourceType);
+        }
+
+        internal Type GetResourceType(string alias)
+        {
+            return base.GetComponent(alias);
+        }
+    }
+}
