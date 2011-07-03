63815adaa701254a493fa056a669488ad8cb2988
maul.esel <maul.esel@go4more.de>
2011-07-03 22:00:25 +0200

    added ILanguageResource interface

---
+++ ChameleonCoder/Resources/ILanguageResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChameleonCoder.Resources
+{
+    public interface ILanguageResource
+    {
+        Guid language { get; }
+        List<Guid> compatibleLanguages { get; }
+    }
+}
