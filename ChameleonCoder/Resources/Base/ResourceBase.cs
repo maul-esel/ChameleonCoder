37d4e4e89b2b56eab7197ce18200ec736f33d1f4
maul.esel <maul.esel@go4more.de>
2011-07-10 01:05:39 +0200

    extended IResource interface, sorted properties + methods

--- ChameleonCoder/Resources/Base/ResourceBase.cs
+++ ChameleonCoder/Resources/Base/ResourceBase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Linq;
-using System.Windows.Controls;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Media;
 using System.Xml;
 using ChameleonCoder.Resources.Collections;
@@ -39,23 +38,85 @@ namespace ChameleonCoder.Resources.Base
 
         public abstract ImageSource Icon { get; }
 
-        /// <summary>
-        /// the GUID that uniquely identifies the resource
-        /// </summary>
         public Guid GUID { get; protected set; }
+
+        public virtual string Name
+        {
+            get
+            {
+                try
+                {
+                    return this.XMLNode.Attributes["name"].Value;
+                }
+                catch (NullReferenceException) { return string.Empty; }
+            }
+            protected set { this.XMLNode.Attributes["name"].Value = value; }
+        }
+
+        public virtual string Description
+        {
+            get
+            {
+                try
+                {
+                    return this.XMLNode.Attributes["description"].Value;
+                }
+                catch (NullReferenceException) { return null; }
+            }
+            protected set { this.XMLNode.Attributes["description"].Value = value; }
+        }
+
+        public virtual string Notes
+        {
+            get
+            {
+                try { return this.XMLNode.Attributes["notes"].Value; }
+                catch (NullReferenceException) { return string.Empty; }
+            }
+            set { this.XMLNode.Attributes["notes"].Value = value; }
+        }
+
+        public virtual MetadataCollection MetaData { get; protected internal set; }
+
+        public virtual ResourceCollection children { get; protected set; }
 
         public virtual ImageSource SpecialVisualProperty { get { return null; } }
 
-        /// <summary>
-        /// saves the information changed through the UI to the current instance and its XML representation
-        /// </summary>
         public virtual void Save()
         {
             foreach (Metadata data in this.MetaData)
             {
                 data.Save(); // changes through the UI should be saved when they occur or through binding
             }
-            this.XMLNode.OwnerDocument.Save(new System.Uri(this.XMLNode.BaseURI).LocalPath);
+            this.XMLNode.OwnerDocument.Save(new Uri(this.XMLNode.BaseURI).LocalPath);
+        }
+
+        public IResource Create() { return null; }
+
+        public bool AddRichContentMember(ChameleonCoder.RichContent.IContentMember member) { return false; }
+
+        public virtual void AttachResource(IResource newChild) { }
+
+        public virtual void AddMetadata()
+        {
+            // ...
+
+            this.Save();
+        }
+
+        public virtual void DeleteMetadata()
+        {
+
+        }
+
+        public virtual void Move()
+        {
+
+        }
+
+        public virtual void Delete()
+        {
+
         }
 
         #endregion 
@@ -74,157 +135,6 @@ namespace ChameleonCoder.Resources.Base
         #endregion
 
         /// <summary>
-        /// the display name of the resource
-        /// </summary>
-        public virtual string Name
-        {
-            get
-            {
-                try
-                {
-                    return this.XMLNode.Attributes["name"].Value;
-                }
-                catch (NullReferenceException) { return string.Empty; }
-            }
-            protected set { this.XMLNode.Attributes["name"].Value = value; }
-        }
-
-        
-
-        /// <summary>
-        /// a short description of the resource
-        /// </summary>
-        public virtual string Description
-        {
-            get
-            {
-                try
-                {
-                    return this.XMLNode.Attributes["description"].Value;
-                }
-                catch (NullReferenceException) { return null; }
-            }
-            protected set { this.XMLNode.Attributes["description"].Value = value; }
-        }
-
-        /// <summary>
-        /// the associated metadata as Metadata class instances
-        /// </summary>
-        public virtual MetadataCollection MetaData { get; protected internal set; }
-
-        /// <summary>
-        /// any notes related to the resource
-        /// </summary>
-        public virtual string Notes
-        {
-            get
-            {
-                try { return this.XMLNode.Attributes["notes"].Value; }
-                catch (NullReferenceException) { return string.Empty; }
-            }
-            set { this.XMLNode.Attributes["notes"].Value = value; }
-        }
-
-        /// <summary>
-        /// a ResourceCollection containing the children resources
-        /// </summary>
-        public virtual ResourceCollection children { get; protected set; }
-
-
-        #region methods
-
-        /// <summary>
-        /// packages a resource into a zip file,
-        /// including all attached or linked resources,
-        /// all data files,
-        /// any file a resource points to (FileResource, CodeResource, LibraryResource)
-        /// </summary>
-        public virtual void Package()
-        {
-
-        }
-
-        
-
-        /// <summary>
-        /// adds a metadata element, given any changes through the UI
-        /// it directly manipulates
-        /// 1) the XmlDocument
-        /// 2) the MetadataCollection
-        /// </summary>
-        internal virtual void AddMetadata()
-        {
-            // ...
-
-            this.Save();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        internal virtual void DeleteMetadata()
-        {
-
-        }
-
-        /// <summary>
-        /// attaches a copy of the resource to another resource
-        /// to do so, a new GUID is created.
-        /// a parameter or user input define where it goes:
-        /// the original resource or the new copy
-        /// </summary>
-        internal virtual void AttachResource() // todo: parameter to define where the guid goes
-        {
-
-        }
-
-        /// <summary>
-        /// receives a resource that should be attached,
-        /// adds it into the tree (-view), the children collection and the XmlDocument
-        /// </summary>
-        internal virtual void ReceiveAttach()
-        {
-
-        }
-
-        /// <summary>
-        /// links a resource to another resource
-        /// </summary>
-        internal virtual void LinkResource()
-        {
-
-        }
-
-        /// <summary>
-        /// receives a resource link
-        /// </summary>
-        internal virtual void ReceiveLink()
-        {
-
-        }
-
-        /// <summary>
-        /// moves a resource to another resource
-        /// by first copying and then deleting it.
-        /// When it is copied, the copy should receive the old GUID
-        /// </summary>
-        internal virtual void Move()
-        {
-
-        }
-
-        /// <summary>
-        /// deletes a resource by removing it from the tree (-view), any Collections and the XmlDocument
-        /// </summary>
-        internal virtual void Delete()
-        {
-            
-        }
-
-        public IResource Create() { System.Windows.MessageBox.Show("creation..."); return null; }
-        public bool AddRichContentMember(ChameleonCoder.RichContent.IContentMember member) { return false; }
-
-        /// <summary>
         /// overrides object.ToString()
         /// </summary>
         /// <returns>the name of the resource</returns>
@@ -232,7 +142,5 @@ namespace ChameleonCoder.Resources.Base
         {
             return this.Name;
         }
-
-        #endregion
     }
 }
--- ChameleonCoder/Resources/Interfaces/IResource.cs
+++ ChameleonCoder/Resources/Interfaces/IResource.cs
@@ -6,24 +6,92 @@ namespace ChameleonCoder.Resources.Interfaces
 {
     public interface IResource
     {
+        /// <summary>
+        /// the resource's icon in the main treeview
+        /// </summary>
         ImageSource Icon { get; }
-
+        
+        /// <summary>
+        /// the unique identifier
+        /// </summary>
         Guid GUID { get; }
 
-        void Save();
-
-        IResource Create();
-
-        bool AddRichContentMember(ChameleonCoder.RichContent.IContentMember member);
-
-        void Package();
-
+        /// <summary>
+        /// the user-defined name of the resource
+        /// Mutilple resources with the same name are allowed.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// a user-defined short description of the resource
+        /// </summary>
         string Description { get; }
 
+        /// <summary>
+        /// any user-defined notes related to the resource
+        /// </summary>
+        string Notes { get; }
+
+        /// <summary>
+        /// the associated metadata as Metadata class instances
+        /// </summary>
+        MetadataCollection MetaData { get; }
+
+        /// <summary>
+        /// a ResourceCollection containing the (direct) child resources
+        /// </summary>
         ResourceCollection children { get; }
 
+        // a RichContent collection
+
+        /// <summary>
+        /// an image representing an important information related to the resource
+        /// </summary>
         ImageSource SpecialVisualProperty { get; }
+
+        /// <summary>
+        /// saves the information changed through the UI to the XML representation
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// creates a new instance of the resource type + saves it to XML
+        /// </summary>
+        /// <returns></returns>
+        IResource Create();
+
+        /// <summary>
+        /// adds a new RichContentMember to the resource
+        /// the resource can validate it beforehand and is "allowed" to refuse
+        /// return false in such cases
+        /// </summary>
+        /// <param name="member">the RichContentMember to add</param>
+        /// <returns>true if the member was added, false otherwise</returns>
+        bool AddRichContentMember(ChameleonCoder.RichContent.IContentMember member);
+
+        /// <summary>
+        /// attaches a new resource to the running instance
+        /// </summary>
+        void AttachResource(IResource newChild);
+
+        /// <summary>
+        /// adds a metadata element, given any changes through the UI
+        /// it directly manipulates
+        /// 1) the Xml-Document
+        /// 2) the MetadataCollection
+        /// </summary>
+        void AddMetadata();
+
+        void DeleteMetadata();
+
+        /// <summary>
+        /// moves a resource to another parent resource
+        /// </summary>
+        void Move();
+
+        /// <summary>
+        /// deletes a resource
+        /// </summary>
+        void Delete();
     }
 }
