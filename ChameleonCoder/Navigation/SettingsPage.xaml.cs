06d9b20446fcb2b517dbc7089235100122911032
maul.esel <maul.esel@go4more.de>
2011-07-23 23:10:47 +0200

    started settings page

---
+++ ChameleonCoder/Navigation/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ChameleonCoder.Navigation
+{
+    /// <summary>
+    /// Interaktionslogik für SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
