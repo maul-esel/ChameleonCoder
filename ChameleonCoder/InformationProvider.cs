4c612595fdba8d273fe33f1f355788b4f7ecd61a
maul.esel <maul.esel@go4more.de>
2011-07-23 18:32:49 +0200

    modified list of ignored files

---
+++ ChameleonCoder/InformationProvider.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ChameleonCoder
+{
+    public static class InformationProvider
+    {
+        public static string ProgrammingDirectory
+        {
+            get
+            {
+                return Properties.Settings.Default.ScriptDir;
+            }
+        }
+    }
+}
