a7286d7842e9612fdeb41d2db19c57942f1eb89a
maul.esel <maul.esel@go4more.de>
2011-07-24 20:39:19 +0200

    prevent changing file extensions when opening the settings tab

--- ChameleonCoder/Navigation/SettingsPage.xaml.cs
+++ ChameleonCoder/Navigation/SettingsPage.xaml.cs
@@ -12,16 +12,23 @@ namespace ChameleonCoder.Navigation
         public SettingsPage()
         {
             InitializeComponent();
-            extInstCheck.SetCurrentValue(CheckBox.IsCheckedProperty, true);
-            //extInstCheck.IsChecked = (Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(".ccm") != null
-            //        && Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(".ccr") != null);
+            ignoreChecking = true;
+            extInstCheck.IsChecked = (Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(".ccm") != null
+                    && Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(".ccr") != null);
         }
+
+        bool ignoreChecking = false;
 
         private void InstallExtensions(object sender, EventArgs e)
         {
-            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(App.AppPath, "--install_ext") { UseShellExecute = true, Verb = "runAs" };
-            try { System.Diagnostics.Process.Start(info); }
-            catch (System.ComponentModel.Win32Exception) { }
+            if (ignoreChecking)
+                ignoreChecking = false;
+            else
+            {
+                System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(App.AppPath, "--install_ext") { UseShellExecute = true, Verb = "runAs" };
+                try { System.Diagnostics.Process.Start(info); }
+                catch (System.ComponentModel.Win32Exception) { }
+            }
         }
     }
 }
