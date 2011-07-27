23d49aaa22b46219ff500c1d7db782e84ec5ed3f
maul.esel <maul.esel@go4more.de>
2011-07-27 18:47:33 +0200

    finished path functions

--- ChameleonCoder/Resources/ResourceHelper.cs
+++ ChameleonCoder/Resources/ResourceHelper.cs
@@ -69,10 +69,43 @@ namespace ChameleonCoder
 
             while (resource != null)
             {
-                path += delimiter + resource.Name + path;
+                path = delimiter + resource.Name + path;
                 resource = resource.Parent;
             }
             return path;
+        }
+
+        public static IResource GetResourceFromPath(string path)
+        {
+            if (path.StartsWith("CC\\"))
+                path.Remove(0, 3);
+            Resources.ResourceCollection collection = Resources.Management.ResourceManager.GetChildren();
+            string[] segments = path.Split(new char[1] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+            IResource result = null;
+            int i = 1;
+            foreach (string segment in segments)
+            {
+                foreach (IResource res in collection)
+                {
+                    System.Windows.MessageBox.Show(segment + "==" + res.Name);
+                    if (res.Name == segment && segments.Length > i)
+                    {
+                        collection = res.children;
+                        break;
+                    }
+                    else if (res.Name == segment && segments.Length == i)
+                    {
+                        result = res;
+                        System.Windows.MessageBox.Show("result!");
+                        break;
+                    }
+                    else
+                        System.Windows.MessageBox.Show("no match");
+                }
+                i++;
+            }
+            return result;
         }
 
         public static string GetPath(this IResource resource)
