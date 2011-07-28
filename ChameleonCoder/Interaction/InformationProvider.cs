ab89e3a5ebc91ebea5ebdce4c9d671c7218d5bac
maul.esel <maul.esel@go4more.de>
2011-07-28 22:43:54 +0200

    added methods important for plugins to InformationProvider

--- ChameleonCoder/Interaction/InformationProvider.cs
+++ ChameleonCoder/Interaction/InformationProvider.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ChameleonCoder.Resources.Management;
+using ChameleonCoder.Resources.Interfaces;
+using ChameleonCoder.Resources;
 
 namespace ChameleonCoder.Interaction
 {
@@ -7,6 +10,14 @@ namespace ChameleonCoder.Interaction
         public static string ProgrammingDirectory { get { return Properties.Settings.Default.ProgrammingDir; } }
         public static int Language { get { return Properties.Settings.Default.Language; } }
 
+        public static IResource GetInstance(Guid id)
+        {
+            return ResourceManager.GetList().GetInstance(id);
+        }
 
+        public static ResourceTypeInfo GetInfo(Type type)
+        {
+            return new ResourceTypeInfo(ResourceTypeManager.GetInfo(type));
+        }
     }
 }
