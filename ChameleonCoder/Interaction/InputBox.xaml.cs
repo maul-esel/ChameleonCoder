59c7b7ee1660c5b6e4edf59d47031aaa5eee796a
maul.esel <maul.esel@go4more.de>
2011-08-09 18:20:52 +0200

    moved InputBox to main project and made it usable for other components

---
+++ ChameleonCoder/Interaction/InputBox.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+
+namespace ChameleonCoder.Interaction
+{
+    /// <summary>
+    /// Interaktionslogik für InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window
+    {
+        public InputBox(string title, string prompt, Func<string, Action<string>, bool> validation)
+        {
+            InitializeComponent();
+            Title = title;
+            Prompt = prompt;
+            _validation = validation;
+        }
+
+        public InputBox(string title, string prompt)
+            : this(title, prompt, (s, e) => true)
+        {
+        }
+
+        public string Prompt
+        {
+            get { return PromptText.Text; }
+            set { PromptText.Text = value; }
+        }
+
+        public Func<string, Action<string>, bool> _validation;
+
+        public string Text
+        {
+            get
+            {
+                if (IsInitialized && DialogResult == null)
+                    return entered.Text;
+                return _text;
+            }
+            set
+            {
+                if (IsInitialized && DialogResult == null)
+                    entered.Text = value;
+            }
+        }
+
+        string _text;
+
+        ChameleonCoder.Interaction.InvalidInputAdorner errorDisplay;
+
+        private void TextEntered(object sender, EventArgs e)
+        {
+            Verify();
+        }
+
+        public bool Verify()
+        {
+            if (_validation != null)
+            {
+                string error = null;
+                if (!_validation(entered.Text, s => error = s))
+                {
+                    errorDisplay = InvalidInputAdorner.Adorn(entered, error);
+                    return false;
+                }
+                else
+                {
+                    if (errorDisplay != null)
+                        errorDisplay.Remove();
+                    entered.ToolTip = null;
+                    return true;
+                }
+            }
+            return true;            
+        }
+
+        public void Close(object sender, EventArgs e)
+        {
+            if (Verify())
+            {
+                _text = entered.Text;
+                DialogResult = true;
+                Close();
+            }
+        }
+    }
+}
--- GuidCreator/CreatorView.xaml.cs
+++ GuidCreator/CreatorView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using ChameleonCoder.Interaction;
 
 namespace GuidCreator
 {
@@ -21,9 +22,23 @@ namespace GuidCreator
 
         public void Enter(object sender, EventArgs e)
         {
-            InputBox box = new InputBox();
+            InputBox box = new InputBox("GuidCreator", "enter a GUID", (s, error) =>
+                {
+                    if (string.IsNullOrWhiteSpace(s))
+                    {
+                        error("GUID must not be empty!");
+                        return false;
+                    }
+                    Guid g;
+                    if (!Guid.TryParse(s, out g))
+                    {
+                        error("You did not enter a valid GUID!");
+                        return false;
+                    }
+                    return true;
+                });
             if (box.ShowDialog() == true)
-                DataContext = currentGuid = box.Guid;
+                DataContext = currentGuid = Guid.Parse(box.Text);
         }
     }
 }
