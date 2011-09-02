131f0da4868d5f4fc1ad0acfac6205d60b875010
maul.esel <maul.esel@go4more.de>
2011-09-02 20:51:01 +0200

    settings: ChameleonCoder.ResourceCore plugin installed by default

--- ChameleonCoder/Properties/Settings.Designer.cs
+++ ChameleonCoder/Properties/Settings.Designer.cs
@@ -98,7 +98,8 @@ namespace ChameleonCoder.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>e6662af6d0fd45bda2ab8784eda3079d</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection InstalledPlugins {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["InstalledPlugins"]));
