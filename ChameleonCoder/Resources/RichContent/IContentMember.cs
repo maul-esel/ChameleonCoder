7e0a303fbbaa9ba1795edf6fbb627b0eef87c38b
maul.esel <maul.esel@go4more.de>
2011-09-02 16:10:53 +0200

    added some more notes to IContentMember

--- ChameleonCoder/Resources/RichContent/IContentMember.cs
+++ ChameleonCoder/Resources/RichContent/IContentMember.cs
@@ -1,5 +1,8 @@
 ﻿namespace ChameleonCoder.Resources.RichContent
 {
+    /// <summary>
+    /// an interface describing a member of the RichContent some resources can have.
+    /// </summary>
     public interface IContentMember
     {
         /// <summary>
@@ -12,6 +15,10 @@
         /// </summary>
         /// <param name="param">optional information for the member</param>
         /// <returns>the member's representation</returns>
+        /// <remarks>the rendering of an member is decided by its parent member.
+        /// For example, the parent member can integrate the html given by GetHtml(),
+        /// but it can also just use a property of the child member and add it,
+        /// or it can even ignore it.</remarks>
         string GetHtml(object param = null);
 
         /// <summary>
