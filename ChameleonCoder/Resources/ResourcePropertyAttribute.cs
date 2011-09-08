c07dd9169b3f91ff0173270ca617ecb7c71567d3
maul.esel <maul.esel@go4more.de>
2011-09-08 23:29:19 +0200

    clean code in ResourcePropertyAttribute

--- ChameleonCoder/Resources/ResourcePropertyAttribute.cs
+++ ChameleonCoder/Resources/ResourcePropertyAttribute.cs
@@ -12,48 +12,46 @@ namespace ChameleonCoder.Resources
             Group = group;
         }
 
-        public ResourcePropertyAttribute(CommonResourceProperty name, ResourcePropertyGroup group)
+        public ResourcePropertyAttribute(CommonResourceProperty property, ResourcePropertyGroup group)
         {
             Group = group;
-
-            string _name = string.Empty;
-            switch (name)
+            switch (property)
             {
-                case CommonResourceProperty.GUID: _name = GUID_Name; break;
-                case CommonResourceProperty.Name: _name = Name_Name; break;
-                case CommonResourceProperty.Description: _name = Description_Name; break;
-                case CommonResourceProperty.Parent: _name = Parent_Name; break;
-                case CommonResourceProperty.CompilationPath: _name = CompilationPath_Name; break;
-                case CommonResourceProperty.FSPath: _name = FSPath_Name; break;
-                case CommonResourceProperty.Language: _name = Language_Name; break;
-                case CommonResourceProperty.CompatibleLanguages: _name = CompatibleLanguages_Name; break;
+                case CommonResourceProperty.GUID: Name = GUID_Name; break;
+                case CommonResourceProperty.Name: Name = Name_Name; break;
+                case CommonResourceProperty.Description: Name = Description_Name; break;
+                case CommonResourceProperty.Parent: Name = Parent_Name; break;
+                case CommonResourceProperty.CompilationPath: Name = CompilationPath_Name; break;
+                case CommonResourceProperty.FSPath: Name = FSPath_Name; break;
+                case CommonResourceProperty.Language: Name = Language_Name; break;
+                case CommonResourceProperty.CompatibleLanguages: Name = CompatibleLanguages_Name; break;
+                default: Name = string.Empty; break;
             }
-            Name = _name;
         }
 
-        public readonly string Name;
-        public readonly ResourcePropertyGroup Group;
+        internal readonly string Name;
+        internal readonly ResourcePropertyGroup Group;
 
         public bool IsReadOnly { get; set; }
         public bool IsReferenceName { get; set; }
 
         #region Names
 
-        public static string GUID_Name { get { return Res.Info_GUID; } }
+        private static string GUID_Name { get { return Res.Info_GUID; } }
 
-        public static string Name_Name { get { return Res.Info_Name; } }
+        private static string Name_Name { get { return Res.Info_Name; } }
 
-        public static string Description_Name { get { return Res.Info_Description; } }
+        private static string Description_Name { get { return Res.Info_Description; } }
 
-        public static string Parent_Name { get { return Res.Info_Parent; } }
+        private static string Parent_Name { get { return Res.Info_Parent; } }
 
-        public static string CompilationPath_Name { get { return Res.Info_CompilationPath; } }
+        private static string CompilationPath_Name { get { return Res.Info_CompilationPath; } }
 
-        public static string FSPath_Name { get { return Res.Info_FSPath; } }
+        private static string FSPath_Name { get { return Res.Info_FSPath; } }
 
-        public static string Language_Name { get { return Res.Info_Language; } }
+        private static string Language_Name { get { return Res.Info_Language; } }
 
-        public static string CompatibleLanguages_Name { get { return Res.Info_CompatibleLanguages; } }
+        private static string CompatibleLanguages_Name { get { return Res.Info_CompatibleLanguages; } }
 
         #endregion
     }
