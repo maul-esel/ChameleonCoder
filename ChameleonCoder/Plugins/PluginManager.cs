370b4ae07069d493c521c0521038c3f848837089
maul.esel <maul.esel@go4more.de>
2011-09-10 15:22:44 +0200

    minor code cleanup

--- ChameleonCoder/Plugins/PluginManager.cs
+++ ChameleonCoder/Plugins/PluginManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ChameleonCoder.Resources.Interfaces;
 using IF = ChameleonCoder.Interaction.InformationProvider;
 
 namespace ChameleonCoder.Plugins
