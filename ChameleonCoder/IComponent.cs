fbe742a8c18b077e17775cfdb41a1d9fb5db52ce
maul.esel <maul.esel@go4more.de>
2011-09-12 22:13:40 +0200

    add Icomponent interface (seems to have been lost somehow)

---
+++ ChameleonCoder/IComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace ChameleonCoder
+{
+    /// <summary>
+    /// a base interface for all external classes CC interacts with
+    /// </summary>
+    public interface IComponent
+    {
+        /// <summary>
+        /// an icon displayed along with the component's name
+        /// </summary>
+        ImageSource Icon { get; }
+
+        /// <summary>
+        /// a Guid uniquely identifying the component
+        /// </summary>
+        Guid Identifier { get; }
+
+        /// <summary>
+        /// the component's display name
+        /// </summary>
+        string Name { get; }
+    }
+}
