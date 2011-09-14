99419cf65f2866c4fc01f20ebd942ba376d09bec
maul.esel <maul.esel@go4more.de>
2011-09-14 22:37:40 +0200

    improve and renew RichContent support: * delete ContentMemberTypeInfo class * update ContentMemberCollection.RegisterMember() to accept an alias instead of a ~TypeInfo instance * update ContentMemberManager to use new IRichContentFactory and to avoit ~TypeInfo * add RichContentManager class (no implementations yet) * update project file

--- ChameleonCoder/Resources/RichContent/ContentMemberCollection.cs
+++ ChameleonCoder/Resources/RichContent/ContentMemberCollection.cs
@@ -4,9 +4,9 @@ namespace ChameleonCoder.Resources.RichContent
 {
     internal sealed class ContentMemberCollection : ComponentCollection<string, Type>
     {
-        internal void RegisterMember(ContentMemberTypeInfo info, Type member)
+        internal void RegisterContentMember(string alias, Type member)
         {
-            base.RegisterComponent(info.Alias, member);
+            base.RegisterComponent(alias, member);
         }
 
         internal Type GetMember(string alias)
--- ChameleonCoder/Resources/RichContent/ContentMemberManager.cs
+++ ChameleonCoder/Resources/RichContent/ContentMemberManager.cs
@@ -1,21 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using ChameleonCoder.Plugins;
 
 namespace ChameleonCoder.Resources.RichContent
 {
-    internal static class ContentMemberManager
+    public static class ContentMemberManager
     {
-        private static ContentMemberCollection ContentMembers = new ContentMemberCollection();
-
-        internal static void RegisterComponent(Type component, ContentMemberTypeInfo info)
-        {
-            if (component.GetInterface(typeof(IContentMember).FullName) != null
-                && !component.IsAbstract && !component.IsInterface && !component.IsNotPublic
-                && !ContentMembers.IsRegistered(info.Alias))
-            {
-                ContentMembers.RegisterMember(info, component);
-            }
-        }
-
         internal static IContentMember CreateInstanceOf(string alias)
         {
             Type member = ContentMembers.GetMember(alias);
@@ -23,5 +13,46 @@ namespace ChameleonCoder.Resources.RichContent
                 return null;
             return Activator.CreateInstance(member) as IContentMember;
         }
+
+        /// <summary>
+        /// the collection holding the RichContent types
+        /// </summary>
+        private static ContentMemberCollection ContentMembers = new ContentMemberCollection();
+
+        /// <summary>
+        /// a dictionary associating the resource types with the registering component factory
+        /// </summary>
+        private static ConcurrentDictionary<Type, IRichContentFactory> Factories = new ConcurrentDictionary<Type, IRichContentFactory>();
+
+        /// <summary>
+        /// registers a RichContent-type
+        /// </summary>
+        /// <param name="member">a Type object representing the registered class</param>
+        /// <param name="alias">the Xml-Alias of the class</param>
+        /// <param name="factory">the factory working with the class</param>
+        public static void RegisterContentMember(Type member, string alias, IRichContentFactory factory)
+        {
+            if (member.GetInterface(typeof(IContentMember).FullName) != null
+                && !member.IsAbstract && !member.IsInterface && !member.IsNotPublic // scope and type
+                && member.GetConstructor(Type.EmptyTypes) != null // creation
+                && !IsRegistered(alias) && !IsRegistered(member) // no double-registration
+                && PluginManager.IsRichContentFactoryRegistered(factory)) // no anonymous registration
+            {
+                ContentMembers.RegisterContentMember(alias, member);
+                Factories.TryAdd(member, factory);
+            }
+        }
+
+        public static bool IsRegistered(string alias)
+        {
+            return ContentMembers.IsRegistered(alias);
+        }
+
+        public static bool IsRegistered(Type type)
+        {
+            return ContentMembers.IsRegistered(type);
+        }
+
+        
     }
 }
