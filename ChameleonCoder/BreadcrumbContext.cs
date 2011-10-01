f4f24d03a91aa31f8b5736659d15f60fc0cf2f27
maul.esel <maul.esel@go4more.de>
2011-10-01 14:59:20 +0200

    BreadcrumbContext: use CCTabPage enumeration

--- ChameleonCoder/BreadcrumbContext.cs
+++ ChameleonCoder/BreadcrumbContext.cs
@@ -1,32 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Windows.Media;
+using ChameleonCoder.Interaction;
 
 namespace ChameleonCoder
 {
     internal sealed class BreadcrumbContext
     {
-        internal BreadcrumbContext(string name, ImageSource icon, IEnumerable children, ContextType type)
+        internal BreadcrumbContext(string name, ImageSource icon, IEnumerable children, CCTabPage pageType)
         {
+            switch (pageType)
+            {
+                case CCTabPage.ResourceEdit:
+                case CCTabPage.ResourceView:
+                case CCTabPage.None:
+                    throw new InvalidOperationException("specified page type is not valid.");
+
+                default:
+                    PageType = pageType;
+                    break;
+            }
             Name = name;
             Icon = icon;
             Children = children;
-            Type = type;
         }
 
-        internal ContextType Type { get; private set; }
+        internal CCTabPage PageType { get; private set; }
 
         public IEnumerable Children { get; private set; }
 
         public ImageSource Icon { get; private set; }
 
         public string Name { get; private set; }
-
-        internal enum ContextType
-        {
-            Home,
-            ResourceList,
-            Settings,
-            Plugins            
-        }
     }    
 }
