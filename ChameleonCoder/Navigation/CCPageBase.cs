8343ed4cb91578067835f0f8c001b14827047af7
maul.esel <maul.esel@go4more.de>
2011-10-02 15:15:02 +0200

    remove useless constructor

--- ChameleonCoder/Navigation/CCPageBase.cs
+++ ChameleonCoder/Navigation/CCPageBase.cs
@@ -2,10 +2,6 @@
 {
     internal abstract class CCPageBase : System.Windows.Controls.Page
     {
-        public CCPageBase()
-        {
-        }
-
         protected CCPageBase(ViewModel.ViewModelBase model)
         {
             DataContext = model;
