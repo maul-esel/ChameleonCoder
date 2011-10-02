f80de28ccd9cdce5a27016d6f4a74195b2216e03
maul.esel <maul.esel@go4more.de>
2011-10-02 15:25:45 +0200

    start file management page

---
+++ ChameleonCoder/Navigation/FileManagementPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChameleonCoder.Navigation
+{
+    /// <summary>
+    /// Interaction logic for FileManagementPage.xaml
+    /// </summary>
+    internal sealed partial class FileManagementPage : CCPageBase
+    {
+        internal FileManagementPage(DataFile file)
+            : base(new ViewModel.FileManagementPageModel(file))
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChameleonCoder/ViewModel/FileManagementPageModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ChameleonCoder.ViewModel
+{
+    internal sealed class FileManagementPageModel : ViewModelBase
+    {
+        internal FileManagementPageModel(DataFile file)
+        {
+            this.file = file;
+        }
+
+        public static IList<DataFile> AllFiles
+        {
+            get
+            {
+                return DataFile.LoadedFiles;
+            }
+        }
+
+        public IDictionary<string, string> Metadata
+        {
+            get
+            {
+                return file.GetMetadata();
+            }
+        }
+
+        private readonly DataFile file;
+    }
+}
