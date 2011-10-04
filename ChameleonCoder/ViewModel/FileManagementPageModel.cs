a5983e4e1605be72700179041c7e1badf161984c
maul.esel <maul.esel@go4more.de>
2011-10-04 20:53:00 +0200

    some more work on file management page & model

--- ChameleonCoder/ViewModel/FileManagementPageModel.cs
+++ ChameleonCoder/ViewModel/FileManagementPageModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Res = ChameleonCoder.Properties.Resources;
 
 namespace ChameleonCoder.ViewModel
 {
@@ -7,6 +9,11 @@ namespace ChameleonCoder.ViewModel
         internal FileManagementPageModel(DataFile file)
         {
             this.file = file;
+
+            Commands.Add(new CommandBinding(ChameleonCoderCommands.DeleteMetadata,
+                DeleteMetadataCommandExecuted));
+            Commands.Add(new CommandBinding(ChameleonCoderCommands.AddMetadata,
+                AddMetadataCommandExecuted));
         }
 
         public static IList<DataFile> AllFiles
@@ -26,5 +33,64 @@ namespace ChameleonCoder.ViewModel
         }
 
         private readonly DataFile file;
+
+        public object ActiveMetadata
+        {
+            get;
+            set;
+        }
+
+        #region localization
+
+        public static string MetadataKey { get { return Res.MetadataKey; } }
+
+        public static string MetadataValue { get { return Res.MetadataValue; } }
+
+        #endregion
+
+        #region commanding
+
+        private void AddMetadataCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            AddMetadata();
+        }
+
+        private void DeleteMetadataCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            e.Handled = true;
+
+            if (ActiveMetadata != null)
+            {
+                var key = ((KeyValuePair<string, string>)ActiveMetadata).Key;
+                if (!string.IsNullOrWhiteSpace(key))
+                {
+                    file.DeleteMetadata(key);
+                    OnPropertyChanged("Metadata");
+                }
+            }
+        }
+
+        #endregion
+
+        private void AddMetadata()
+        {
+            var name = OnUserInput(Res.Status_CreateMeta, Res.Meta_EnterName);
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                OnReport(Res.Status_CreateMeta, Res.Error_MetaInvalidName, Interaction.MessageSeverity.Error);
+                return;
+            }
+            else if (file.GetMetadata(name) != null)
+            {
+                OnReport(Res.Status_CreateMeta, Res.Error_MetaDuplicateName, Interaction.MessageSeverity.Error);
+                return;
+            }
+
+            file.SetMetadata(name, null);
+            OnPropertyChanged("Metadata");
+        }
     }
 }
