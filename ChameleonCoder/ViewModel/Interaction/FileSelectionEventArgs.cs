b822f776c7b97011a6209e128bc3f6454129d6c3
maul.esel <maul.esel@go4more.de>
2011-10-09 16:28:17 +0200

    add new FileSelectionEventArgs class

---
+++ ChameleonCoder/ViewModel/Interaction/FileSelectionEventArgs.cs
@@ -0,0 +1,51 @@
+﻿namespace ChameleonCoder.ViewModel.Interaction
+{
+    internal sealed class FileSelectionEventArgs : System.EventArgs
+    {
+        internal FileSelectionEventArgs(string message)
+            : this(message, System.Environment.CurrentDirectory, true)
+        {
+        }
+
+        internal FileSelectionEventArgs(string message, bool mustExist)
+            : this(message, System.Environment.CurrentDirectory, true)
+        {
+        }
+
+        internal FileSelectionEventArgs(string message, string dir)
+            : this(message, dir, true)
+        {
+        }
+
+        internal FileSelectionEventArgs(string message, string dir, bool mustExist)
+        {
+            Message = message;
+            Directory = dir;
+            MustExist = mustExist;
+        }
+
+        internal string Message
+        {
+            get;
+            private set;
+        }
+
+        internal string Directory
+        {
+            get;
+            private set;
+        }
+
+        internal bool MustExist
+        {
+            get;
+            private set;
+        }
+
+        internal string Path
+        {
+            get;
+            set;
+        }
+    }
+}
