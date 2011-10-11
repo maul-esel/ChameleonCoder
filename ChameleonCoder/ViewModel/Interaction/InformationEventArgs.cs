e244bfcaabbc4fc7c8cebc8435a18b97f0926168
maul.esel <maul.esel@go4more.de>
2011-10-11 17:29:49 +0200

    add generic InformationEventArgs<T> class

---
+++ ChameleonCoder/ViewModel/Interaction/InformationEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace ChameleonCoder.ViewModel.Interaction
+{
+    public sealed class InformationEventArgs<T> : System.EventArgs
+    {
+        internal InformationEventArgs() { }
+
+        /// <summary>
+        /// the information the sender receives
+        /// </summary>
+        public T Information
+        {
+            get;
+            set;
+        }
+    }
+}
