979d6d07f0a5aa6858fff77a4917bf67ecf65bb7
maul.esel <maul.esel@go4more.de>
2011-10-12 22:49:29 +0200

    add comment

--- ChameleonCoder/ViewModel/Interaction/InformationEventArgs.cs
+++ ChameleonCoder/ViewModel/Interaction/InformationEventArgs.cs
@@ -1,5 +1,9 @@
 ﻿namespace ChameleonCoder.ViewModel.Interaction
 {
+    /// <summary>
+    /// a class that can be used to obtain typed information from a model client
+    /// </summary>
+    /// <typeparam name="T">the type of information to be obtained</typeparam>
     public sealed class InformationEventArgs<T> : System.EventArgs
     {
         internal InformationEventArgs() { }
