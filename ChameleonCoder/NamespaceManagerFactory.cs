98781fc6aa977cca6be823a3f7e7eab14f948d5b
maul.esel <maul.esel@go4more.de>
2011-10-12 22:40:06 +0200

    add 'NamespaceManagerFactory' class to avoid several instances of XmlNamepsaceManager where 1 would be enough

---
+++ ChameleonCoder/NamespaceManagerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace ChameleonCoder
+{
+    internal static class NamespaceManagerFactory
+    {
+        internal static XmlNamespaceManager GetManager(XmlDocument doc)
+        {
+            if (loadedManagers.ContainsKey(doc))
+                return loadedManagers[doc];
+
+            var manager = new XmlNamespaceManager(doc.NameTable);
+            manager.AddNamespace("cc", DataFile.NamespaceUri);
+            loadedManagers.Add(doc, manager);
+
+            return manager;
+        }
+
+        internal static void ClearManagers()
+        {
+            loadedManagers.Clear();
+        }
+
+        private static Dictionary<XmlDocument, XmlNamespaceManager> loadedManagers = new Dictionary<XmlDocument, XmlNamespaceManager>();
+    }
+}
