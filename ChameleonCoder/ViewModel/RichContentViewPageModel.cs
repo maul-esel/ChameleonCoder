5f273ea0a3c13a6212b6e0467492bca6fe2b57db
maul.esel <maul.esel@go4more.de>
2011-10-28 18:11:03 +0200

    add model for RichContentViewPage

---
+++ ChameleonCoder/ViewModel/RichContentViewPageModel.cs
@@ -0,0 +1,20 @@
+﻿using ChameleonCoder.Resources.Interfaces;
+
+namespace ChameleonCoder.ViewModel
+{
+    [DefaultRepresentation(typeof(Navigation.RichContentViewPage))]
+    internal sealed class RichContentViewPageModel : ViewModelBase
+    {
+        internal RichContentViewPageModel(IRichContentResource resource)
+        {
+            Resource = resource;
+        }
+
+        internal IRichContentResource Resource { get; private set; }
+
+        public string Markup
+        {
+            get { return Resource.GetHtml(); }
+        }
+    }
+}
