888fb98dcb9591511dfabef6774371fa04786474
maul.esel <maul.esel@go4more.de>
2011-10-28 15:17:45 +0200

    add basic RichContent page

---
+++ ChameleonCoder/Navigation/RichContentViewPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ChameleonCoder.Navigation
+{
+    /// <summary>
+    /// Interaction logic for RichContentViewPage.xaml
+    /// </summary>
+    internal sealed partial class RichContentViewPage : System.Windows.Controls.Page
+    {
+        internal RichContentViewPage(string html)
+        {
+            InitializeComponent();
+            NavigationService.Navigate(html);
+        }
+    }
+}
