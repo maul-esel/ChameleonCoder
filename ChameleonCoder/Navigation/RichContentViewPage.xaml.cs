460262bfc9ef48a7278f9b486a2fc75f104525b5
maul.esel <maul.esel@go4more.de>
2011-10-28 18:13:40 +0200

    implement showing RichContent HTML

--- ChameleonCoder/Navigation/RichContentViewPage.xaml.cs
+++ ChameleonCoder/Navigation/RichContentViewPage.xaml.cs
@@ -1,14 +1,23 @@
-﻿namespace ChameleonCoder.Navigation
+﻿using ChameleonCoder.Resources.Management;
+
+namespace ChameleonCoder.Navigation
 {
     /// <summary>
     /// Interaction logic for RichContentViewPage.xaml
     /// </summary>
     internal sealed partial class RichContentViewPage : System.Windows.Controls.Page
     {
-        internal RichContentViewPage(string html)
+        internal RichContentViewPage(ViewModel.RichContentViewPageModel model)
         {
+            ModelClientHelper.InitializeModel(model);
+
+            ResourceManager.Open(model.Resource);
+
+            DataContext = model;
+            CommandBindings.AddRange(model.Commands);
+
             InitializeComponent();
-            NavigationService.Navigate(html);
+            frame.NavigateToString(model.Markup);
         }
     }
 }
