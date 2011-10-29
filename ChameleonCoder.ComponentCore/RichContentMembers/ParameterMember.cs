a0cbd62f74e0e62fb2af2cc8737c630262ffc3d6
maul.esel <maul.esel@go4more.de>
2011-10-29 23:22:40 +0200

    add some properties for parameters

--- ChameleonCoder.ComponentCore/RichContentMembers/ParameterMember.cs
+++ ChameleonCoder.ComponentCore/RichContentMembers/ParameterMember.cs
@@ -19,6 +19,24 @@ namespace ChameleonCoder.ComponentCore.RichContentMembers
         {
         }
 
+        /// <summary>
+        /// gets a parameter's type
+        /// </summary>
+        public string Type
+        {
+            get { return Xml.GetAttribute("param-type", DataFile.NamespaceUri); }
+            protected set { Xml.SetAttribute("param-type", DataFile.NamespaceUri, value); }
+        }
+
+        /// <summary>
+        /// gets a parameter's default value
+        /// </summary>
+        public string DefaultValue
+        {
+            get { return Xml.GetAttribute("default-value", DataFile.NamespaceUri); }
+            protected set { Xml.SetAttribute("default-value", DataFile.NamespaceUri, value); }
+        }
+
         internal new const string Key = "{d78144be-71b4-46f9-a947-4a5e3bf0caa9}";
     }
 }
