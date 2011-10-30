0979fb981147f444c39a977c09208e94a3bb9dc1
maul.esel <maul.esel@go4more.de>
2011-10-30 20:12:50 +0100

    add new 'ConstantMember' class

---
+++ ChameleonCoder.ComponentCore/RichContentMembers/ConstantMember.cs
@@ -0,0 +1,24 @@
+﻿using ChameleonCoder.Resources.Interfaces;
+using ChameleonCoder.Resources.RichContent;
+
+namespace ChameleonCoder.ComponentCore.RichContentMembers
+{
+    public class ConstantMember : VariableMember
+    {
+        public ConstantMember(System.Xml.XmlElement data, IContentMember parent, IRichContentResource resource)
+            : base(data, parent, resource)
+        {
+        }
+
+        public override string GetHtml(object param)
+        {
+            return base.GetHtml(param);
+        }
+
+        public string Value
+        {
+            get { return Xml.GetAttribute("value", DataFile.NamespaceUri); }
+            protected set { Xml.SetAttribute("value", DataFile.NamespaceUri, value); }
+        }
+    }
+}
