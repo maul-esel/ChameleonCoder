9d77ae8edd045ab52485823dbe39af7f62887465
maul.esel <maul.esel@go4more.de>
2011-11-04 12:58:12 +0100

    add CompiledResource class

---
+++ ChameleonCoder.ComponentCore/Resources/CompiledResource.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using ChameleonCoder.Plugins;
+using ChameleonCoder.Resources;
+using ChameleonCoder.Resources.Interfaces;
+using ChameleonCoder.Resources.RichContent;
+
+namespace ChameleonCoder.ComponentCore.Resources
+{
+    /// <summary>
+    /// represents a file compiled from source code
+    /// </summary>
+    public class CompiledResource : FileResource, ILanguageResource, IRichContentResource
+    {
+        /// <summary>
+        /// initializes the current instance with the given information
+        /// </summary>
+        /// <param name="data">the XmlElement containing the resource's definition</param>
+        /// <param name="parent">the resource's parent resource,
+        /// or null if the resource is a top-level resource.</param>
+        public override void Update(XmlElement data, IResource parent)
+        {
+            base.Update(data, parent);
+            // todo: parse compatible languages
+        }
+
+        #region ILanguageResource
+
+        /// <summary>
+        /// gets the identifier of the language module in whose coding language the source code is written.
+        /// </summary>
+        /// <value>The value is taken from the "language" attribute in the resource's XML.</value>
+        public Guid Language
+        {
+            get
+            {
+                Guid lang;
+                string guid = Xml.GetAttribute("language", DataFile.NamespaceUri);
+
+                if (!Guid.TryParse(guid, out lang))
+                    lang = Guid.Empty;
+
+                return lang;
+            }
+            protected set
+            {
+                Xml.SetAttribute("language", DataFile.NamespaceUri, value.ToString("b"));
+                OnPropertyChanged("Language");
+            }
+        }
+
+        /// <summary>
+        /// gets a list of Guid's identifying language modules
+        /// to whose coding languages this resource is compatible
+        /// </summary>
+        /// <value>(not yet implemented)</value>
+        public IEnumerable<Guid> CompatibleLanguages
+        {
+            get { return languages; }
+        }
+
+        private List<Guid> languages = new List<Guid>();
+
+        #endregion
+
+        #region IRichContentResource
+
+        /// <summary>
+        /// gets the representation of the resource's RichContent
+        /// </summary>
+        /// <returns>the HTML as string</returns>
+        public string GetHtml()
+        {
+            string markup = null;
+
+            var list = System.Linq.Enumerable.OrderBy(RichContent, c => c.GetType().Name);
+            foreach (var member in list)
+            {
+                markup += member.GetHtml(null);
+            }
+
+            return "<!DOCTYPE html>\n<html><head><style type='text/css'>"
+                + StyleSheet
+                + "</style></head><body style='margin: 2.5px; border: solid gray 2px; padding: 2.5px; font-family: Calibri, MS Sans Serif, sans-serif'>"
+                + markup
+                + "</body></html>";
+        }
+
+        /// <summary>
+        /// registers a CSS class style
+        /// </summary>
+        /// <param name="classStyle">the style to register</param>
+        public void RegisterClassStyle(ChameleonCoder.Resources.RichContent.Css.CssClassStyle classStyle)
+        {
+            StyleSheet += classStyle.ToString() + '\n';
+        }
+
+        private string StyleSheet = null;
+
+        /// <summary>
+        /// contains the resource's RichContent members
+        /// </summary>
+        public RichContentCollection RichContent
+        {
+            get { return collection; }
+        }
+
+        private RichContentCollection collection = new RichContentCollection();
+
+        #endregion
+
+        #region PropertyAliases
+
+        /// <summary>
+        /// gets the display name of this resource's language module
+        /// </summary>
+        [ResourceProperty(CommonResourceProperty.Language, ResourcePropertyGroup.ThisClass, IsReadOnly = true)]
+        public string LanguageName
+        {
+            get
+            {
+                ILanguageModule module;
+                if (PluginManager.TryGetModule(Language, out module))
+                    return module.Name;
+                return "error: module could not be found";
+            }
+        }
+
+        /// <summary>
+        /// gets a string contatining the dislay names of the language modules to which the source code is compatible.
+        /// </summary>
+        [ResourceProperty(CommonResourceProperty.CompatibleLanguages, ResourcePropertyGroup.ThisClass, IsReadOnly = true)]
+        public string CompatibleLanguagesNames
+        {
+            get
+            {
+                string list = string.Empty;
+                foreach (Guid lang in CompatibleLanguages)
+                {
+                    ILanguageModule module;
+                    if (PluginManager.TryGetModule(lang, out module))
+                        list += module.Name + "; ";
+                }
+                return list;
+            }
+        }
+
+        #endregion
+
+        internal new const string Key = "{20bde16c-4201-452d-af66-215734686fa7}";
+    }
+}
