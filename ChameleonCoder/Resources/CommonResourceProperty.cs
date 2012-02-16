86929f58b9425a227805a4037b7da13afa54ba1a
maul.esel <maul.esel@go4more.de>
2012-02-16 17:59:36 +0100

    move enum classes to own files

---
+++ ChameleonCoder/Resources/CommonResourceProperty.cs
@@ -0,0 +1,43 @@
+﻿namespace ChameleonCoder.Resources
+{
+    /// <summary>
+    /// defines common properties of resource types
+    /// </summary>
+    public enum CommonResourceProperty
+    {
+        /// <summary>
+        /// the unique identifier of the resource
+        /// </summary>
+        Identifier,
+
+        /// <summary>
+        /// the name of the resource
+        /// </summary>
+        Name,
+
+        /// <summary>
+        /// the resource's description
+        /// </summary>
+        Description,
+
+        /// <summary>
+        /// the resource's parent
+        /// </summary>
+        Parent,
+
+        /// <summary>
+        /// the resource's file system path (for <see cref="ChameleonCoder.Resources.Interfaces.IFSComponent"/> resources)
+        /// </summary>
+        FSPath,
+
+        /// <summary>
+        /// the resource's coding language (for <see cref="ChameleonCoder.Resources.Interfaces.ILanguageResource"/> resources)
+        /// </summary>
+        Language,
+
+        /// <summary>
+        /// the resource's compatible languages (for <see cref="ChameleonCoder.Resources.Interfaces.ILanguageResource"/> resources)
+        /// </summary>
+        CompatibleLanguages
+    }
+}
---
+++ ChameleonCoder/Resources/ResourcePropertyGroup.cs
@@ -0,0 +1,28 @@
+﻿namespace ChameleonCoder.Resources
+{
+    /// <summary>
+    /// an enumeration defining in which property group to place a property
+    /// with the <see cref="ChameleonCoder.Resources.ResourcePropertyAttribute"/> applied on it.
+    /// </summary>
+    public enum ResourcePropertyGroup
+    {
+        /// <summary>
+        /// place the property in the "General" group.
+        /// </summary>
+        /// <remarks>You should only use this for properties
+        /// corresponding to <see cref="ChameleonCoder.Resources.Interfaces.IResource"/> properties.</remarks>
+        General,
+
+        /// <summary>
+        /// the display name of the class declaring the property will be used
+        /// </summary>
+        /// <remarks>If the declaring type is not registered,
+        /// the <see cref="CurrentClass"/> value will be used instead.</remarks>
+        ThisClass,
+
+        /// <summary>
+        /// the display name of the class the resource is an instance of will be used.
+        /// </summary>
+        CurrentClass
+    }
+}
