09db818c594c057073794df66585bb1895ce0452
maul.esel <maul.esel@go4more.de>
2012-02-16 18:43:17 +0100

    add new RegistryManager class

---
+++ ChameleonCoder/RegistryManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+
+namespace ChameleonCoder
+{
+    internal static class RegistryManager
+    {
+        public static readonly string fileExtension = ".ccr";
+
+        /// <summary>
+        /// registers the file extension
+        /// </summary>
+        internal static void RegisterExtension()
+        {
+            if (Registry.ClassesRoot.OpenSubKey(".ccr") == null)
+            {
+                RegistryKey regCCR = Registry.ClassesRoot.CreateSubKey(fileExtension, RegistryKeyPermissionCheck.ReadWriteSubTree);
+                regCCR.SetValue("", ChameleonCoder.Properties.Resources.Ext_CCR);
+                regCCR.Close();
+
+                regCCR = Registry.ClassesRoot.CreateSubKey(fileExtension + "\\Shell\\Open\\command");
+                regCCR.SetValue("", "\"" + App.AppPath + "\" \"%1\"");
+                regCCR.Close();
+
+                regCCR = Registry.ClassesRoot.CreateSubKey(fileExtension + "\\DefaultIcon");
+                regCCR.SetValue("", App.AppPath + ", 1");
+                regCCR.Close();
+            }
+        }
+
+        /// <summary>
+        /// unregisters the file extension
+        /// </summary>
+        internal static void UnRegisterExtension()
+        {
+            if (Registry.ClassesRoot.OpenSubKey(".ccr") != null)
+            {
+                Registry.ClassesRoot.DeleteSubKeyTree(fileExtension);
+            }
+        }
+    }
+}
